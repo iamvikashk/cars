cc22ae959c9bf080d5c2b278cfd2ef12bb6a6633
Vikash <vickyraj1521@gmail.com>
2022-09-20 15:51:25 -0700

    Resolution for issue - Price not available

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+import re
 
 hide_table_row_index = """
             <style>
@@ -26,20 +27,27 @@ if submit_button:
     mileage_all = []
     price_all = []
     name_all = []
+    url_all = []
     for car in cars:
+        url = car.find(href=re.compile("/vehicledetail.+?/"), class_="vehicle-card-link")
         year = int(car.find("h2", class_="title").text.strip()[0:5])
         mileage = int(car.find("div", class_="mileage").text.strip()[0:-3].replace(',',''))
-        price = int(car.find("span", class_="primary-price").text.strip()[1:].replace(',',''))
+        if car.find("span", class_="primary-price").text.strip() == "Not Priced":
+            price = 0
+        else:
+            price = int(car.find("span", class_="primary-price").text.strip()[1:].replace(',',''))
         name = car.find("h2", class_="title").text.strip()
         year_all.append(year)
         mileage_all.append(mileage)
         price_all.append(price)
         name_all.append(name)
-    data = pd.DataFrame({"year":year_all, "mileage":mileage_all, "price": price_all, "name": name_all})
+        url_all.append("https://www.cars.com" + url['href'])
+    data = pd.DataFrame({"year":year_all, "mileage":mileage_all, "price": price_all, "name": name_all, "url": url_all})
+    data = data[data["price"] > 0]
     data["year_rank"] = data["year"].rank()
     data["mileage_rank"] = data["mileage"].rank(ascending=False)
     data["price_rank"] = data["price"].rank(ascending=False)
     data["avg_rank"] = round((data["year_rank"] + data["mileage_rank"] + data["price_rank"])/3)
     data.drop(columns=["year_rank", "mileage_rank", "price_rank"], inplace=True)
     data.sort_values(by=["avg_rank", "price"], ascending=[False, True], inplace=True)
-    st.table(data.drop(columns="avg_rank"))
+    st.table(data.drop(columns=["avg_rank", "url"]))
